0606e68ad72b6cb265cd248cdbedff88b1e61b6c
Vova <volodymyrkachuriak@gmail.com>
2025-10-27 11:03:00 +0100

    refactor: separate maven-hook from the main project

--- ci-cd-merge-resolver/src/test/java/ch/unibe/cs/mergeci/service/MergeAnalyzerTest.java
+++ ci-cd-merge-resolver/src/test/java/ch/unibe/cs/mergeci/service/MergeAnalyzerTest.java
@@ -13,8 +13,8 @@ class MergeAnalyzerTest {
     @Test
     void buildProjects() throws Exception {
         MergeAnalyzer mergeAnalyzer = new MergeAnalyzer("src/test/resources/test-merge-projects/jackson-databind", "temp");
-        mergeAnalyzer.buildProjects("bf08f05406f90cd6a3e76e76687dfe45b22105d5", "a36a049147c023becffbea2793042caef3ca3285", "7613683b4ab806924cfb44438eed416b1e302438");
-        mergeAnalyzer.runTests();
+//        mergeAnalyzer.buildProjects("bf08f05406f90cd6a3e76e76687dfe45b22105d5", "a36a049147c023becffbea2793042caef3ca3285", "7613683b4ab806924cfb44438eed416b1e302438");
+//        mergeAnalyzer.runTests();
 
         System.out.println("Compilation result:");
         Map<String, CompilationResult> compilationResultMap = mergeAnalyzer.collectCompilationResults();
